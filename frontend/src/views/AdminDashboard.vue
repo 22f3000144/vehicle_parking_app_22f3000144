9aa20aa729495c4c98df04f42c0f5e79e15f136d
Ayus-Gupta <22f3000144@ds.study.iitm.ac.in>
2025-10-28 20:15:17 +0530

    Milestone-VP-MAD2 Admin-Dashboard-Management

--- frontend/src/views/AdminDashboard.vue
+++ frontend/src/views/AdminDashboard.vue
@@ -3,6 +3,9 @@
     <h3>Welcome, Admin!</h3>
     <p>You can manage parking lots, users, and view reports.</p>
     <button @click="logout" class="btn btn-danger mt-3">Logout</button>
+    <button @click="addparkinglot" > + Add Parking </button>
+
+
   </div>
 </template>
 
