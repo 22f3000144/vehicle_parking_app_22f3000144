2c3d75cc8974510f028d4bcf92c1f027c471a827
Ayus-Gupta <22f3000144@ds.study.iitm.ac.in>
2025-11-29 18:25:23 +0530

    Completed..

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,13 +3,17 @@
 </script>
 
 <template>
+  <header>
+    Parking Application
+  </header>
+
    <img alt="Vue logo" class="logo" src="../assets/logo.png" width="150" height="150" />
 
     <div>
       <nav>
         <RouterLink to="/register">Sine-In</RouterLink><br>
         <!-- <RouterLink to="/about">About</RouterLink> -->
-        <RouterLink to="/userlogin">User-Login</RouterLink>
+        <RouterLink to="/userlogin">User-Login</RouterLink><br>
         <RouterLink to="/adminlogin">Admin-Login</RouterLink>
 
       </nav>
@@ -17,5 +21,39 @@
 </template>
 
 <style>
+:root{
+  --color-1:#005840;   /* 60% */
+  --color-2:#D1F843;   /* 30% */
+  --color-3:#ECF0EF;   /* 10% */
+}
+
+
+
+/* Header bar */
+header{
+  text-align:center;
+  padding:1rem 0;
+  font-size:1.6rem;
+  color:#fff;
+  font-weight:600;
+  background: var(--color-1);
+  border-bottom:3px solid var(--color-2);
+}
+
+/* Center logo + nav */
+.logo {
+  display:block;
+  margin:2rem auto 1rem auto;
+}
+
+/* Navigation */
+nav{
+  text-align:center;
+  margin-top:1rem;
+  line-height: 2.2rem;
+}
+
+
+
 
 </style>
